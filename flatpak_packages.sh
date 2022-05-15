46573c94e0077485fe0f2538a121754f3a735e68
Amir Pourmand <pourmand1376@gmail.com>
2022-05-15 20:50:26 +0430

    add flatpak_packages

---
+++ flatpak_packages.sh
@@ -0,0 +1,6 @@
+flatpak install ch.openboard.OpenBoard
+flatpak install com.anydesk.Anydesk
+flatpak install org.telegram.desktop
+flatpak install org.kde.kdenlive
+flatpak install com.github.joseexposito.touche
+
