0cd62c864c0d09566e77b9e237f967489d6236d2
Amir Pourmand <pourmand1376@gmail.com>
2022-05-27 15:55:10 +0430

    add

--- flatpak_packages.sh
+++ flatpak_packages.sh
@@ -1,5 +1,5 @@
 flatpak install ch.openboard.OpenBoard -y
-flatpak install com.anydesk.Anydesk -y
+# flatpak install com.anydesk.Anydesk -y
 flatpak install org.telegram.desktop -y
 flatpak install org.kde.kdenlive -y
 flatpak install com.github.joseexposito.touche -y
@@ -10,9 +10,15 @@ flatpak install flathub com.microsoft.Edge -y
 flatpak install flathub org.telegram.desktop -y
 
 flatpak install flathub com.skype.Client -y
-flatpak install flathub com.visualstudio.code -y
+# flatpak install flathub com.visualstudio.code -y
 flatpak install flathub md.obsidian.Obsidian -y
 
 flatpak install flathub net.agalwood.Motrix -y
 flatpak install flathub org.videolan.VLC -y
 flatpak install flathub info.smplayer.SMPlayer -y
+
+flatpak install flathub org.gnome.Calculator -y
+flatpak install flathub org.kde.kclock -y
+
+flatpak install flathub net.codeindustry.MasterPDFEditor -y
+# flatpak install flathub org.texstudio.TeXstudio -y
