0019938bb70d60a38fa2a50fbc60dfaf7f8dd8d0
Amir Pourmand <pourmand1376@gmail.com>
2022-05-27 23:00:15 +0430

    add miniconda

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -67,4 +67,6 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 
-
+# install
+conda init zsh
+conda init bash
