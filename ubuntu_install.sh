60a24d990ecaef3e43d2471effbc93352dde1572
Amir Pourmand <pourmand1376@gmail.com>
2022-05-27 11:51:05 +0430

    add ubuntu

---
+++ ubuntu_install.sh
@@ -0,0 +1,40 @@
+sudo apt update
+
+# then install nvidia-drivers from additional drivers tab
+
+# to switch between nvidia and prime use
+
+
+# install kitty from here
+# https://sw.kovidgoyal.net/kitty/binary/
+
+# sudo prime-select nvidia or sudo prime-select intel
+sudo apt instal curl git zsh
+mkdir -p ~/.plugins/
+
+
+# here I should put terminal settings
+
+# make kitty default terminal emulator
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which kitty` 50
+sudo update-alternatives --config x-terminal-emulator
+
+
+sudo apt install most
+export PAGER='most' >> .profile
+
+
+
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+sudo apt install neofetch
+sudo apt install neovim 
+sudo apt install kdiff3
+
+
+git config --global merge.tool "kdiff3"
+git config --global user.name "Amir Pourmand"
+git config --global user.email "pourmand1376@gmail.com"
