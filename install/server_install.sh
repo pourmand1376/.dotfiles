e819110fd102c504de78d91577594c74154e0188
Amir Pourmand <pourmand1376@gmail.com>
2022-08-08 14:04:27 +0430

    add brew

--- install/server_install.sh
+++ install/server_install.sh
@@ -5,3 +5,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install --lts
 
 npm i -g jsdu
+
+
+# Just install brew as a package manager and then install everything else
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zsh
