e8a1c9cf897dcfbe184f6b3ad82d69b7ea8f09bf
Amir Pourmand <pourmand1376@gmail.com>
2022-08-18 20:41:17 +0430

    add some server commands

--- install/server_install.sh
+++ install/server_install.sh
@@ -1,15 +1,13 @@
-# https://github.com/nvm-sh/nvm
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-nvm install --lts
-
-npm i -g jsdu
-
-
 # Just install brew as a package manager and then install everything else
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install zsh
 brew install fzf
 brew install ncdu
+
+brew install stow
+
+brew install neovim
+brew install z # for tracking history and smart cd
+
+# also you should use `realpath` command to see full path for a given item
