8037ec432f8fdc21acbb060d1b6d30c3ddb7cd5a
Amir Pourmand <pourmand1376@gmail.com>
2022-09-13 13:51:58 -0700

    add more windows commands

--- install/windows_install.bat
+++ install/windows_install.bat
@@ -5,6 +5,23 @@ winget install BraveSoftware.BraveBrowser
 winget install Microsoft.PowerToys -s winget
 winget install ShareX.ShareX
 
-choco install winbtrfs
-
 winget install -e peazip
+
+winget install -e --id Daum.PotPlayer
+
+# having GNU utils on windows 
+winget install -e --id bmatzelle.Gow 
+
+winget install -e --id Docker.DockerDesktop
+
+winget install -e --id OBSProject.OBSStudio
+
+winget install -e --id KDE.KDEConnect
+winget install -e --id Zoom.Zoom
+winget install -e --id Microsoft.Skype
+
+winget install -e --id Google.Chrome
+
+winget install -e --id Microsoft.VisualStudioCode
+
+wsl --install
