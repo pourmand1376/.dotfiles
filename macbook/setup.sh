57d341eea7c34142195c5e755a5ac91ab50528dd
Amir Pourmand <pourmand1376@gmail.com>
2024-03-28 09:08:07 +0330

    add macbook

---
+++ macbook/setup.sh
@@ -0,0 +1,12 @@
+
+
+
+## brew installs 
+brew install ffmpeg
+brew install --cask alfred
+brew install --cask rectangle
+brew install --cask visual-studio-code
+brew install --cask obsidian
+
+
+# brew install --cask cheatsheat problem 
