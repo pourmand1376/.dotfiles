aa3388aec79c474a78dd1376bcca3c118b68fc2d
Amir Pourmand <pourmand1376@gmail.com>
2024-03-28 09:12:36 +0330

    install firefox

--- macbook/setup.sh
+++ macbook/setup.sh
@@ -7,6 +7,7 @@ brew install --cask alfred
 brew install --cask rectangle
 brew install --cask visual-studio-code
 brew install --cask obsidian
+brew install --cask firefox
 
 
 # brew install --cask cheatsheat problem 
