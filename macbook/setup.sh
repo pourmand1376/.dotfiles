52376d82ae18150237e48e131696881918a4dd3d
Amir Pourmand <pourmand1376@gmail.com>
2024-04-02 23:51:08 +0330

    update dotfiles

--- macbook/setup.sh
+++ macbook/setup.sh
@@ -1,14 +1,15 @@
-
+# https://www.youtube.com/watch?v=GK7zLYAXdDs
 
 
 ## brew installs 
 brew install ffmpeg
-brew install --cask alfred
-brew install --cask rectangle
+# brew install --cask alfred (replaced with raycast)
+# brew install --cask rectangle (replaced with raycast)
 brew install --cask visual-studio-code
 brew install --cask obsidian
 brew install --cask firefox
 brew install --cask maczip
 brew install --cask arc
-brew install --cask maccys
+# brew install --cask maccys (raycast replaced it)
+brew install --cask raycast
 # brew install --cask cheatsheat problem 
