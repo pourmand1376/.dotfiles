30028e9481c9f2da471dd5bd01ff7ee39557fd23
Amir Pourmand <pourmand1376@gmail.com>
2024-04-18 14:30:02 +0330

    .

--- macbook/setup.sh
+++ macbook/setup.sh
@@ -25,3 +25,14 @@ brew install --cask bartender # customize dock icons
 # install oh-my-zsh
 # تقویم روزگار
 # https://apps.apple.com/us/app/roozegar-%D8%B1%D9%88%D8%B2%DA%AF%D8%A7%D8%B1/id1171425651
+
+# V2Box
+# https://apps.apple.com/us/app/v2box-v2ray-client/id6446814690
+
+# V2rayU
+# warp
+# postman
+# pycharm ce
+# docker
+# bitwarden
+# DBeaver
