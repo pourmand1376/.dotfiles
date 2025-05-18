4e1060d96e619573427ba333b9733f259dfbdfb4
Amir Pourmand <pourmand1376@gmail.com>
2025-05-18 11:45:35 +0330

    Update .gitignore and setup script; add Edge window detection and improve comments

--- macbook/setup.sh
+++ macbook/setup.sh
@@ -90,3 +90,5 @@ brew install orbstack ## alternative to docker desktop
 brew install pearcleaner # app cleaner 
 
 # ticktick, obsidian, chatgpt
+
+https://apps.apple.com/us/app/ticktick-to-do-list-calendar/id966085870?mt=12
