5d4116948b3b6d02bea40df4b93ad8cdcd6ecba8
Amir Pourmand <pourmand1376@gmail.com>
2025-08-14 20:23:29 +0330

    Enhance workspace configuration by adding exec-on-workspace-change script and updating workspace assignments for Code and Tasks. Remove redundant window detection for Read workspace.

---
+++ aerospace/pip-move.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+current_monitor=$(aerospace list-monitors --focused | awk '{print $1}')
+current_workspace=$(aerospace list-workspaces --focused)
+win_list=$(aerospace list-windows --monitor "$current_monitor" | grep -E "CleanShot" | awk '{print $1}')
+
+echo "$win_list" | while IFS= read -r number; do
+  echo "Processing number: $number"
+  aerospace move-node-to-workspace --window-id "$number" "$current_workspace" < /dev/null
+  echo "continue"
+done
