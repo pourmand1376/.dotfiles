7c4015a2c2ac5b34651e3d6b98d83d9d9aaf4063
Amir Pourmand <pourmand1376@gmail.com>
2026-07-06 14:52:05 +0330

    update mine

--- macbook/dockutils.sh
+++ macbook/dockutils.sh
@@ -1,10 +1,10 @@
 dockutil --remove all
 dockutil --add /System/Applications/Apps.app/
 dockutil --add /System/Applications/Mail.app/
+dockutil --add /Applications/Discord.app/
 dockutil --add /Applications/TickTick.app/
 dockutil --add /System/Applications/Calendar.app/
 dockutil --add /Applications/Obsidian.app/
-dockutil --add /Applications/Discord.app/
-dockutil --add /Applications/Google Chrome.app/
-dockutil --add /Applications/Microsoft Edge.app/
+dockutil --add '/Applications/Google Chrome.app/'
+dockutil --add '/Applications/Microsoft Edge.app/'
 dockutil --add /Applications/WezTerm.app/
