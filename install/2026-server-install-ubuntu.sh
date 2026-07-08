528a893c294f7c748102636d72a5b42fa9caa8df
Amir Pourmand <pourmand1376@gmail.com>
2026-07-08 15:11:19 +0330

    update git files

--- install/2026-server-install-ubuntu.sh
+++ install/2026-server-install-ubuntu.sh
@@ -1,8 +1,20 @@
 # new server install for apt. considering everything important.
 #
 #
+# First clone my .dotfiles directory into home
+#
 sudo apt update -y
 
-sudo apt install stow build-essential
+sudo apt install stow build-essential neofetch
 
 sudo apt install curl git zsh neovim
+
+# install starship zsh
+curl -sS https://starship.rs/install.sh | sh
+
+stow common
+stow starship
+stow zsh
+stow tmux
+stow nvim
+stow lazygit
