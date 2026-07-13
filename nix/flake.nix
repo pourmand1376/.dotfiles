bccd52579f38fd0d7533855964c0aca1e74e8a68
Amir Pourmand <pourmand1376@gmail.com>
2026-07-13 18:21:14 +0330

    add flake.nix

---
+++ nix/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Amir CLI Packages";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "my-packages";
+
+        paths = with pkgs; [
+          git
+          ripgrep
+          fd
+          jq
+          bat
+          tmux
+        ];
+      };
+    };
+}
